7ff8b03ca7449da92f79bd367d9fef966736794e
Nacim BOUSSAHA <nacim.boussaha@epitech.eu>
2022-11-22 13:41:47 +0100

    update script

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,9 +2,13 @@
 # Docker entrypoint script.
 
 # Sets up tables and running migrations.
+echo "Setting up database..."
 /app/bin/miniproject01 eval "ApiProject.Release.migrate"
+echo "Seeding database..."
 /app/bin/miniproject01 eval "ApiProject.Release.seeds"
+echo "Seed complete."
 /app/bin/miniproject01 eval "ApiProject.Release.migrate"
+echo  "Database setup complete."
 
 # Start our app
 /app/bin/server
